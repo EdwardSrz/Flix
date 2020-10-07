0bcfa29a03abfb5bcf327c0366036037523f1956
Eduardo Suarez <eduardosuarezaday@gmail.com>
2020-10-07 00:45:22 -0400

    Add Superhero and Movie Details

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Eduardo Suarez on 10/6/20.
+//  Copyright © 2020 Xcode Proyects. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
